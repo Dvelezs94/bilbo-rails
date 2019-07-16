8d41deebbf9c15a221070d0112c2b8993820222b
Diego Velez <diegovelzs94@gmail.com>
2019-07-15 21:20:38 -0500

    added migrations and initial stuff

---
+++ db/migrate/20190716004830_create_boards.rb
@@ -0,0 +1,19 @@
+class CreateBoards < ActiveRecord::Migration[5.2]
+  def change
+    create_table :boards do |t|
+      t.references :user, foreign_key: true
+      t.float :latitude
+      t.float :longitude
+      t.integer :avg_daily_views
+      t.float :width
+      t.float :height
+      t.integer :duration
+      t.integer :status
+      # north face, shout, etc
+      t.integer :face
+      t.string :uuid
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20190716005423_create_campaigns.rb
@@ -0,0 +1,16 @@
+class CreateCampaigns < ActiveRecord::Migration[5.2]
+  def change
+    create_table :campaigns do |t|
+      t.references :user, foreign_key: true
+      t.string :name
+      t.string :description
+      t.string :artwork
+      t.float :budget
+      t.integer :status
+      t.datetime :starts_at
+      t.datetime :ends_at
+
+      t.timestamps
+    end
+  end
+end
