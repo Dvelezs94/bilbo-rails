37cd6962fb37d32fb8e44ae6cd846a3fcaa93cac
Diego Velez <diegovelzs94@gmail.com>
2019-07-22 19:13:45 -0500

    changed bilbos views

--- db/migrate/20190716004830_create_boards.rb
+++ db/migrate/20190716004830_create_boards.rb
@@ -2,13 +2,14 @@ class CreateBoards < ActiveRecord::Migration[5.2]
   def change
     create_table :boards do |t|
       t.references :user, foreign_key: true
-      t.float :latitude
-      t.float :longitude
+      t.float :lat
+      t.float :lng
       t.integer :avg_daily_views
       t.float :width
       t.float :height
       t.integer :duration
       t.integer :status
+      t.string :description
       # north face, south, etc
       t.integer :face
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,21 +12,22 @@ if ENV.fetch("RAILS_ENV") != "production"
       user.email = Faker::Internet.email + "#{x}"
       user.company_name = Faker::Company.name
       user.password = "1234aA"
-      puts "#{user.email} - #{user.company_name}"
+      puts "#{user.email}"
     end
   end
 
   100.times do |x|
     Board.create! do |board|
       board.user_id = Faker::Number.between(1, 19)
-      board.latitude = Faker::Address.latitude
-      board.longitude = Faker::Address.longitude
+      board.lat = Faker::Address.latitude
+      board.lng = Faker::Address.longitude
       board.avg_daily_views = Faker::Number.number(6)
       board.width = "#{Faker::Number.between(10, 14)}.#{Faker::Number.between(30, 90)}".to_f
       board.height = "#{Faker::Number.between(7, 9)}.#{Faker::Number.between(30, 90)}".to_f
       board.duration = Faker::Number.within(7..10)
       board.status = Faker::Number.between(0, 2)
       board.face = Faker::Number.between(0, 3)
+      board.description = Faker::Lorem.paragraphs
     end
   end
 end
