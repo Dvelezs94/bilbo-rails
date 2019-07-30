15dc2d693dc43fe4df7504b25c6a56880959451a
Diego Velez <diegovelzs94@gmail.com>
2019-07-29 20:24:04 -0500

    several changes

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -3,4 +3,8 @@ class Ad < ApplicationRecord
   has_and_belongs_to_many :campaigns
   enum status: { pending: 0, accepted: 1, declined: 2}
   has_many_attached :multimedia
+
+  validates :name, presence: true
+
+  validates :multimedia, content_type: ["image/png", "image/jpeg", "image/gif"]
 end
