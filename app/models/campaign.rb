58598023a31fbead3c755343371caca3cb0a0964
Diego Velez <diegovelzs94@gmail.com>
2019-10-16 18:51:03 -0500

    added deleted status to campaigns

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -7,12 +7,13 @@ class Campaign < ApplicationRecord
   belongs_to :ad, optional: true
   has_and_belongs_to_many :boards
   # status is for internal status, like in review, accepted or denied
-  enum status: { just_created: 0, in_review: 1, approved: 2, denied: 3 }
+  enum status: { just_created: 0, in_review: 1, approved: 2, denied: 3, deleted: 4 }
 
   # 'state' is for user desired state of the campaign, enabled or disabled
 
   # Trigger broadcast or remove campaign
   before_update :update_broadcast
+  before_destroy :remove_campaign
 
   validates :name, presence: true
   before_save :set_in_review, :if => :ad_id_changed?
@@ -25,6 +26,11 @@ class Campaign < ApplicationRecord
   def ongoing?
     # validates if both fields are complete
     !(self.starts_at? && self.ends_at?)
+  end
+
+  # Function to know if the campaign has multimedia files in the ad
+  def has_multimedia?
+    ad.present? && ad.multimedia.first.present?
   end
 
   def set_in_review
@@ -45,13 +51,11 @@ class Campaign < ApplicationRecord
 
   # Publish ad function, this gets triggered when the state and status are true
   def publish_campaign
-    p("enabling")
     AdBroadcastWorker.perform_async(self.id, "enable")
   end
 
   # Remove ad function, this gets triggered when the state or status are false
   def remove_campaign
-    p("disabling")
     AdBroadcastWorker.perform_async(self.id, "disable")
   end
 end
