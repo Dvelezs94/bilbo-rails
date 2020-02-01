b5b35eac9609747a5155aca450bb0c21c3b0191d
Diego Velez <diegovelezs94@gmail.com>
2020-02-01 17:14:57 -0600

    finish style for provider general statistics

--- app/assets/javascripts/provider/statistics_charts.js
+++ app/assets/javascripts/provider/statistics_charts.js
@@ -88,4 +88,40 @@ $(document).on('turbolinks:load', function() {
       chartdonut.draw(datadonut, optionsdonut);
     }
   }
+
+  // Geneeral metrics chart
+  if ($("#chartBar1").length) {
+    google.charts.setOnLoadCallback(drawVisualization);
+
+    function drawVisualization() {
+      // Some raw data (not necessarily accurate)
+      var datageneral = google.visualization.arrayToDataTable([
+        ['Month', 'Bolivia', 'Ecuador', 'Madagascar', 'Papua New Guinea', 'Rwanda', 'Average'],
+        ['2004/05', 165, 938, 522, 998, 450, 614.6],
+        ['2005/06', 135, 1120, 599, 1268, 288, 682],
+        ['2006/07', 157, 1167, 587, 807, 397, 623],
+        ['2007/08', 139, 1110, 615, 968, 215, 609.4],
+        ['2008/09', 136, 691, 629, 1026, 366, 569.6]
+      ]);
+
+      var optionsgeneral = {
+        hAxis: {
+          title: "Date"
+        },
+        vAxis: {
+          title: "Earnings"
+        },
+        seriesType: 'bars',
+        height: "100%",
+        width: "100%",
+        chartArea: {
+          height: "75%",
+          width: "75%"
+        }
+      };
+
+      var chart = new google.visualization.ComboChart($("#chartBar1")[0]);
+      chart.draw(datageneral, optionsgeneral);
+    }
+  }
 });
