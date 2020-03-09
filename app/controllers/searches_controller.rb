c35ac41b080720e68afbc99ebe5653b106a1422e
Pedro <pitpedrosalo@gmail.com>
2020-03-09 13:30:38 -0600

    Add bilbo from admin

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,3 +1,7 @@
 class SearchesController < ApplicationController
-  autocomplete :user, :email, :extra_data => [:roles], :full => true
+  def autocomplete_user_email
+    term = params[:term]
+    users = User.where("email LIKE '%#{term}%' AND roles= 'provider'")
+    render :json => users.map { |user| {:id => user.id, :label => user.email, :value => user.email} }
+  end
 end
