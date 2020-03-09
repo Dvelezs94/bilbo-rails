0f246a924444fcbd30485186a3bbb57857d17b13
Pedro <pitpedrosalo@gmail.com>
2020-03-09 11:01:06 -0600

    Autocomplete display working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,16 @@
 //= require rails-ujs
 //= require activestorage
-//= require jquery3
-//= require jquery.steps
-//= require jquery-ui
-//= require jquery-ui/widgets/autocomplete
-//= require autocomplete-rails
 //= require turbolinks
 
+//= require jquery3
 //= require popper
 //= require bootstrap
-//= require gritter
+//= require jquery.steps
+//= require jquery-ui
 //= require clipboard
 //= require data-confirm-modal
+//= require jquery-ui/widgets/autocomplete
+//= require autocomplete-rails
+
 //= require_tree ./global_dependencies
 //= require_tree ./shared
