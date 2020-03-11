806452bea07a2afbd9d4cad59c25b27855cb3297
EmRam <emram@pop-os.localdomain>
2020-03-11 11:16:28 -0600

    "save"

--- app/workers/provider_impressions_csv_worker.rb
+++ app/workers/provider_impressions_csv_worker.rb
@@ -5,19 +5,24 @@ class ProviderImpressionsCsvWorker
   
     def perform(user_id)
       @user = User.find(user_id)
+      name = "impressions-#{Time.now.strftime("%Y%m%d")}"
       generate_csv 
+      send_csv
     end
 
 private
 
     def generate_csv 
-      attributes = %w["campaign", "board", "created_at", "total_price" ]
+      attributes = %w["campaign", "board", "created_at", "total_price", "name" ]
       require 'csv'
-        CSV.generate(headers: true) do |csv|
+        CSV.open("tmp/report.csv") do |csv|
           csv << attributes 
+          Report.create.attachment
         end
       end
-    
+      def send_csv(csv)
+        send_data csv, :type => 'text/csv; charset=utf-8; header=present', :disposition => 'attachment; filename=impressions.csv'
+    end
 
 end
   
