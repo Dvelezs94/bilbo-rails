0f55b12cdcfb26073c92cf0a20a1d02244bfb3a2
Diego Velez <diegovelezs94@gmail.com>
2020-03-11 21:31:28 -0600

    fix devise

--- app/models/project_user.rb
+++ app/models/project_user.rb
@@ -7,7 +7,6 @@ class ProjectUser < ApplicationRecord
   enum role: { owner: 0, administrator: 1, user: 2 }
 
   before_validation :set_user_id, if: :email?
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   def set_user_id
     self.user = User.invite!(email: email, project_name: email.split("@")[0])
