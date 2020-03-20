d7b1b0ab1ebd594de63077dde6831fa103ea6eb6
Diego Velez <diegovelezs94@gmail.com>
2020-03-20 10:29:06 -0600

    fix tubolink loading effect

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,30 +1,18 @@
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
-
 //= require jquery3
-//= require popper
-//= require bootstrap
-//= require toastr
 //= require jquery.steps
 //= require jquery-ui
+//= require jquery-ui/widgets/autocomplete
+//= require turbolinks
+
+//= require popper
+//= require_tree ./shared
+//= require bootstrap
+//= require toastr
 //= require clipboard
 //= require data-confirm-modal
-//= require jquery-ui/widgets/autocomplete
 //= require autocomplete-rails
 //= require chartkick
 
 //= require_tree ./global_dependencies
-
-
-$(document).on('turbolinks:fetch', function(){
-  $("#p1").removeClass("d-none");
-  $(".content").hide();
-  $(".spinner").show();
-});
-
-$(document).on("turbolinks:receive", function(){
-  $("#p1").addClass("d-none");
-  $(".spinner").hide();
-  $(".content").show();
-});
