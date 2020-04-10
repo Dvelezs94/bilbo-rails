1da783dcc9a5ab5acc147c7319aa0baac0f2e412
Diego Velez <diego.velez@bairesdev.com>
2020-04-10 12:53:12 -0500

    chartkick refactor

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,10 +1,20 @@
 class ChartsController < ApplicationController
   access user: :all
+  before_action :get_campaign, only: [:daily_impressions, :daily_invested, :peak_hours]
 
+  # Campaign Charts
   def daily_impressions
-    get_campaign
     render json: @campaign.daily_impressions
   end
+
+  def daily_invested
+    render json: @campaign.daily_invested
+  end
+
+  def peak_hours
+    render json: @campaign.peak_hours
+  end
+  # End Campaign Charts
 
   private
 
