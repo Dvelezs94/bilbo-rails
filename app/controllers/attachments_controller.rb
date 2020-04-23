bdf954cd4f8eddf4a8ee3244c2b7a7d50b2edb8a
Pedro <pitpedrosalo@gmail.com>
2020-04-23 13:16:35 -0500

    transitions multimedia

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -26,6 +26,16 @@ class AttachmentsController < ApplicationController
     redirect_to ad_path(@ad)
   end
 
+  def update
+    if Ad.find(params[:ad_id]).multimedia.find_by_id(params[:id]).update(transition:params[:effect])
+      flash[:success] = I18n.t('ads.transition_ad')
+    else
+      flash[:error] = I18n.t('ads.errors.wont_be_able_to_update')
+    end
+    redirect_to ad_path(Ad.find(params[:ad_id]))
+  end
+
+
   private
 
   def get_ad
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_195831) do
+ActiveRecord::Schema.define(version: 2020_04_21_230444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2020_04_20_195831) do
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
-    t.string "transition", default: "none"
+    t.integer "transition", default: 0
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
