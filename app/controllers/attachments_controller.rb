fbc482eaf88df96b9cd231f0ec05d19c14403ab7
Pedro <pitpedrosalo@gmail.com>
2020-04-28 11:37:02 -0500

    change to select transition

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,7 +1,7 @@
 class AttachmentsController < ApplicationController
   access user: :all, provider: :all
-  before_action :get_ad, only: [:create, :destroy]
-  skip_before_action :verify_authenticity_token, only: [:create]
+  before_action :get_ad, only: [:create, :destroy, :update]
+  skip_before_action :verify_authenticity_token, only: [:create, :update]
 
   def create
     if @ad.campaigns.all_off && @ad.multimedia.attach(params[:files])
@@ -27,14 +27,13 @@ class AttachmentsController < ApplicationController
   end
 
   def update
-    if Ad.find(params[:ad_id]).multimedia.find_by_id(params[:id]).update(transition:params[:effect])
-      flash[:success] = I18n.t('ads.transition_ad')
-    else
-      flash[:error] = I18n.t('ads.transition_no_added')
+    if @ad.present?
+        @ad.multimedia.find_by_id(params[:id]).update(transition:params[:effect])
+        respond_to do |format|
+        format.html{head :no_content}
+      end
     end
-    redirect_to ad_path(Ad.find(params[:ad_id]))
   end
-
 
   private
 
@@ -44,4 +43,5 @@ class AttachmentsController < ApplicationController
     @ad.multimedia_update = true
     @ad
   end
+
 end
