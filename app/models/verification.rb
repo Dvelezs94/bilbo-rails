5602bb9dadbc3805081ac8b0625ebcc52e9ab9e6
Diego Velez <diego.velez@bairesdev.com>
2020-05-09 10:27:59 -0500

    fixed some verification issues

--- app/models/verification.rb
+++ app/models/verification.rb
@@ -5,7 +5,7 @@ class Verification < ApplicationRecord
 
   has_one_attached :official_id
   validates :official_id, content_type: ["image/png", "image/jpeg", "image/jpg"]
-  validates_presence_of :name, :user_id, :official_id, :business_name, :street_1, :city, :state, :zip_code, :business_code, :official_business_name, :phone
+  validates_presence_of :name, :user_id, :official_id, :business_name, :street_1, :city, :state, :zip_code, :phone
 
   def full_address
     "#{self.street_1} #{self.street_2} #{self.zip_code}. #{self.city} #{self.state}, #{self.country}"
