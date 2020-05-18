af8aae7bac37ad479759eb13a3044ef341ae9c31
Diego Velez <diego.velez@bairesdev.com>
2020-05-18 17:08:34 -0500

    added secrets manager support

---
+++ .docker/secretsmanager.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# call this script by doing ./secretsmanager.sh secret/of/app
+
+secrets=$(aws secretsmanager get-secret-value --secret-id $1 | jq -r '.SecretString')
+#echo $secrets
+for s in $(echo $secrets | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" ); do
+    export $s
+done
