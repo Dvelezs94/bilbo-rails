1b7d07b1589087586ade56bfbb186b706d3c6019
Diego Velez <diego.velez@bairesdev.com>
2020-05-18 17:10:13 -0500

    comment unneeded part of code

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,8 +2,8 @@
 
 if [ -n "$SECRETS_MANAGER_ID" ]; then
   # Get current region on the instance and export it as default region
-  CURRENT_REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
-  export AWS_DEFAULT_REGION=$CURRENT_REGION
+  # CURRENT_REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
+  # export AWS_DEFAULT_REGION=$CURRENT_REGION
   # Get secrets manager secret and export all vaoues as env vars
   secretsmanager $SECRETS_MANAGER_ID
 fi
