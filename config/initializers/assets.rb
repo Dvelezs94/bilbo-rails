d0ace806f3893a331bd625dc796973027d8d08c5
Diego Velez <diego.velez@bairesdev.com>
2020-05-21 08:43:50 -0500

    add missing assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,8 +11,9 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js )
-# Rails.application.config.assets.precompile += %w( board.js )
-# Rails.application.config.assets.precompile += %w( provider.js )
-# Rails.application.config.assets.precompile += %w( provider.js )
-Rails.application.config.assets.precompile += %w( config/* images/* bootstrap/* fonts/* stylesheets/* javascripts/* )
+Rails.application.config.assets.precompile += %w( admin.js )
+Rails.application.config.assets.precompile += %w( board.js )
+Rails.application.config.assets.precompile += %w( provider.js )
+Rails.application.config.assets.precompile += %w( provider.js )
+# add missing assets
+Rails.application.config.assets.precompile += %w( config/* images/* fonts/* )
