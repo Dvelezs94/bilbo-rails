e2262b19fab33d2ca4da52ad58c752089e909f0e
Diego Velez <diego.velez@bairesdev.com>
2020-05-22 11:46:55 -0500

    fixed paypal and verification

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -12,7 +12,8 @@ class PaymentsController < ApplicationController
       allow_guest_checkout: false,
       items: [{name: "Bilbo Credits", description: "#{payment_params_express[:total]} Credits Purchase", quantity: payment_params_express[:total].to_i, amount: 100}]
     )
-    # p response
+    puts "x" * 500
+    puts response.to_s
     redirect_to EXPRESS_GATEWAY.redirect_url_for(response.token)
   end
 
