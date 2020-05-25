64f267df2f7b762af8f75a2218b3342ec6c8f991
bilbo <pitpedrosalo@gmail.com>
2020-05-25 13:21:18 -0500

    select project id

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,7 +3,7 @@ class SearchesController < ApplicationController
   def autocomplete_user_email
     term = params[:term]
     users = User.where("email LIKE '%#{term}%' AND roles= 'provider'")
-    render :json => users.map { |user| {:id => user.id, :label => user.email, :value => user.email} }
+    render :json => users.map { |user| {:id => user.projects.first.id, :label => user.email, :value => user.email} }
   end
 
   # autocompletes user when checking users on admin
