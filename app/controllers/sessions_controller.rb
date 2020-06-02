0127bb626d0b036630c165d4d452c0cd9df97839
bilbo <pitpedrosalo@gmail.com>
2020-06-02 14:59:36 -0500

    added flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,12 @@
 class SessionsController < Devise::SessionsController
   def create
     super
+    flash[:success] = I18n.t('devise.sessions.signed_in')
   end
 
   # GET /resource/sign_out
   def destroy
     super
+    flash[:success] = I18n.t('devise.sessions.signed_out')
   end
 end
