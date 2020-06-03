35e113aa484d631d137f27938ec6508621503f7e
bilbo <pitpedrosalo@gmail.com>
2020-06-03 16:29:28 -0500

    added flash messages, changed time in active/disable campaign, add styles to password and  confirmation devise

--- app/controllers/passwords_controller.rb
+++
@@ -1,7 +0,0 @@
-class PasswordsController < Devise::PasswordsController
-  protected
-  def after_sending_reset_password_instructions_path_for(resource_name)
-    flash[:success] = I18n.t('devise.passwords.send_instructions')
-      new_session_path(resource_name) if is_navigational_format?
-  end
-end
