06219584400078bb888ae626097fda6f40f37c04
bilbo <pitpedrosalo@gmail.com>
2020-06-03 16:17:06 -0500

    added flash messages, changed time in active/disable campaign, add css to password confirmation devise

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -97,7 +97,7 @@ class Campaign < ApplicationRecord
 
   def state_change_time
     # Value for state to be changed on prod every 2 minutes
-    minutes_needed = (ENV["RAILS_ENV"] == "development")? 0.1.minutes : 0.5.minutes
+    minutes_needed = (ENV["RAILS_ENV"] == "development")? 0.1.minutes : 0.2.minutes
     #if state_updated_at is nil, is the first time they update
     time_elapsed = (state_updated_at.present?)? ((Time.now - state_updated_at)/1.minutes).minutes : minutes_needed
     errors.add(:base, "#{I18n.t ('campaign.wont_be_able_to_update_state')} #{distance_of_time_in_words( (minutes_needed - time_elapsed).ago, Time.now, include_seconds: true )}") if (time_elapsed < minutes_needed)
