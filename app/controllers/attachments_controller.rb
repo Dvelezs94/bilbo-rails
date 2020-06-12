0b8631ba9f4bb6fc2283092119e81c555043d9fe
bilbo <pitpedrosalo@gmail.com>
2020-06-12 18:17:24 -0500

    Added ffmpeg, worker video converter and ads accept MP4 and webm

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -4,7 +4,13 @@ class AttachmentsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create, :update]
 
   def create
-    if @ad.campaigns.all_off && @ad.multimedia.attach(params[:files])
+    if @ad.campaigns.all_off
+      if (params[:name].include? "mp4") || (params[:name].include? "webm")
+          @ad.multimedia.attach(params[:files]).each do |video|
+            VideoConverter.perform_async(@ad.id, video.id)
+          end
+      elsif ((!params[:name].include? "mp4") || (!params[:name].include? "webm")) && @ad.multimedia.attach(params[:files])
+    end
       flash[:success] = "Attachment saved"
     else
       flash[:error] = I18n.t('ads.errors.wont_be_able_to_update')
