0e92aa0c903e0ec7c74829afdbaaf067cb6ac72f
Mauricio Velez <mauriciovelsa99@gmail.com>
2020-06-17 17:55:38 -0500

    fixed stuff

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -26,6 +26,7 @@ class CampaignsController < ApplicationController
     @campaign_boards =  @campaign.boards.enabled.collect { |board| ["#{board.address} - #{board.face}", board.id, { 'data-price': board.cycle_price } ] }
     @campaign.starts_at = @campaign.starts_at.to_date rescue ""
     @campaign.ends_at = @campaign.ends_at.to_date rescue ""
+    @boards = Board.all
   end
 
   def toggle_state
