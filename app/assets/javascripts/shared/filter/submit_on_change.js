693f2937edc507ce5f2aa263f7448dec116fdc03
Mauricio Velez <mauriciovelsa99@gmail.com>
2020-06-17 19:03:04 -0500

    added filters campaign edit filters

--- app/assets/javascripts/shared/filter/submit_on_change.js
+++ app/assets/javascripts/shared/filter/submit_on_change.js
@@ -3,10 +3,10 @@ $(document).on('turbolinks:load', function() {
     //dont send form when location changes and not select one
     if (event.target.id != "search_autocomplete") $(this).find("input[type=submit]").click();
   });
-  $( "form#filter-bilbos input[type=submit]" ).on("click", function(event) {
+  $( "form#filter-bilbos input[type=submit]" ).on("click", function(event) { //display loading
     window.maploading = toastr.info("Cargando", "", {"extendedTimeOut": 0, timeOut: 0, tapToDismiss: false});
   });
-  $( "button[form=filter-bilbos]").on("click",function(){
+  $( "button[form=filter-bilbos]").on("click",function(){ //display loading when using the modal
     $( "form#filter-bilbos input[type=submit]" ).click();
   });
 });
