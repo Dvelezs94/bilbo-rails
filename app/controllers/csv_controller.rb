8263da6ab75b53ab69a2cb39f9ab0f6dc9cade8f
Diego Velez <diego.velez@bairesdev.com>
2020-06-29 09:17:05 -0500

    sidekiq 6

--- app/controllers/csv_controller.rb
+++ app/controllers/csv_controller.rb
@@ -5,7 +5,7 @@ class CsvController < ApplicationController
   before_action :validate_daily_hour_generation, only:[:generate_campaign_report, :generate_board_provider_report, :generate_campaign_provider_report]
 
   def generate_provider_report
-    ProviderImpressionsCsvWorker.perform_async( @project.slug, current_user.id)
+    ProviderImpressionsCsvWorker.perform_async(@project.slug, current_user.id)
     flash[:success] = I18n.t('dashboards.reports.report_created')
     redirect_to root_path
   end
@@ -15,21 +15,21 @@ class CsvController < ApplicationController
     @campaign = params[:campaign]
     @start_date = params[:start_date]
     @end_date = params[:end_date]
-    ProviderImpressionsCsvWorker.perform_async( @project.slug, current_user.id, @campaign, @board, @start_date, @end_date)
+    ProviderImpressionsCsvWorker.perform_async(@project.slug, current_user.id, @campaign, @board, @start_date, @end_date)
     flash[:success] = I18n.t('dashboards.reports.report_created')
     redirect_to impressions_path
   end
 
   def generate_campaign_report
     @campaign = params[:campaign]
-    ProviderImpressionsCsvWorker.perform_async( @project.slug, current_user.id, @campaign)
+    ProviderImpressionsCsvWorker.perform_async(@project.slug, current_user.id, @campaign)
     flash[:success] = I18n.t('dashboards.reports.report_created')
     redirect_to analytics_campaign_path(Campaign.find(@campaign_id).slug)
   end
 
   def generate_board_provider_report
     @board = params[:board]
-    ProviderImpressionsCsvWorker.perform_async( @project.slug, current_user.id, nil, @board)
+    ProviderImpressionsCsvWorker.perform_async(@project.slug, current_user.id, nil, @board)
     flash[:success] = I18n.t('dashboards.reports.report_created')
     redirect_to owned_boards_path
   end
