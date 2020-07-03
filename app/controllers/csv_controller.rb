de01d2dca341bcd39ddb306dce9511dbc5b12581
Diego Velez <diego.velez@bairesdev.com>
2020-07-02 22:03:49 -0500

    payments and board changes

--- app/controllers/csv_controller.rb
+++ app/controllers/csv_controller.rb
@@ -67,7 +67,6 @@ class CsvController < ApplicationController
   end
 
   def download_csv
-
     if @project.reports.find_by_name(params[:reference]).present?
       send_file "tmp/#{params[:reference]}", type: 'text/csv'
     end
