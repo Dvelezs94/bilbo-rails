eff951a34ada85899f7e5cda1d475115dc7dc01c
Diego Velez <diego.velez@bairesdev.com>
2020-07-05 22:34:54 -0500

    fixed paypal fee

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,6 +2,7 @@ class Payment < ApplicationRecord
   belongs_to :user
   has_one :invoice
   validates :express_token, uniqueness: true
+  require 'json'
   def purchase
     response = EXPRESS_GATEWAY.purchase(total * 100, express_purchase_options)
     if response.success?
@@ -29,7 +30,9 @@ class Payment < ApplicationRecord
     if new_record? && !token.blank?
       # you can dump details var if you need more info from buyer
       details = EXPRESS_GATEWAY.details_for(token)
-      self.total = details.params["order_total"].to_i
+      credits_total = JSON.pretty_generate(details.params["PaymentDetails"]["PaymentDetailsItem"])
+      #puts get_bilbo_hash_quantity(JSON.parse(credits_total))
+      self.total = get_bilbo_hash_quantity(JSON[credits_total]).to_i
       self.first_name = details.params["first_name"]
       self.last_name = details.params["last_name"]
       self.express_payer_id = details.payer_id
@@ -37,6 +40,16 @@ class Payment < ApplicationRecord
   end
 
   private
+
+  def get_bilbo_hash_quantity(items_array)
+    items_array.each do |elem|
+     if I18n.transliterate(elem["Name"]).downcase.include? "credit"
+       puts elem
+      return elem["Quantity"]
+      break
+     end
+    end
+  end
 
   def express_purchase_options
    {
