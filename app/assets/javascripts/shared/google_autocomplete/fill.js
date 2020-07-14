ad4558bfbe09b416080d33264b80393cd2492d23
Diego Velez <diego.velez@bairesdev.com>
2020-07-14 10:42:54 -0500

    fix map shit

--- app/assets/javascripts/shared/google_autocomplete/fill.js
+++ app/assets/javascripts/shared/google_autocomplete/fill.js
@@ -1,22 +1,22 @@
-$(document).on('turbolinks:load', function() {
-  //location autocompletes needs this events to better ui experience
-  autocom_loc_func(".google_autocomplete");
-});
-
-//this is still useful on google because it autocompletes
-function autocom_loc_func(id) {
-  waitForElement(id, function() {
-  //remove info on click
-  $(id).on("focus", function(e) {
-    window.location_val = $(this).val();
-    $(this).val("");
-  });
-
-  $(id).blur(function(e) {
-      //if no option selected, retype location
-      if ($(this).val() != window.location_val) {
-        $(this).val(window.location_val);
-     }
-  });
-});
-}
+// $(document).on('turbolinks:load', function() {
+//   //location autocompletes needs this events to better ui experience
+//   autocom_loc_func(".google_autocomplete");
+// });
+//
+// //this is still useful on google because it autocompletes
+// function autocom_loc_func(id) {
+//   waitForElement(id, function() {
+//   //remove info on click
+//   $(id).on("focus", function(e) {
+//     window.location_val = $(this).val();
+//     $(this).val("");
+//   });
+//
+//   $(id).blur(function(e) {
+//       //if no option selected, retype location
+//       if ($(this).val() != window.location_val) {
+//         $(this).val(window.location_val);
+//      }
+//   });
+// });
+// }
