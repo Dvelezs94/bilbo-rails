9fd65c337322cab388548ece57ba39beed8cacd9
Diego Velez <diego.velez@bairesdev.com>
2020-07-17 19:46:34 -0500

    external sources setup for fire tv stick

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_161742) do
+ActiveRecord::Schema.define(version: 2020_07_14_222737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -179,6 +179,7 @@ ActiveRecord::Schema.define(version: 2020_07_08_161742) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "transaction_fee"
     t.index ["user_id"], name: "index_payments_on_user_id"
   end
 
