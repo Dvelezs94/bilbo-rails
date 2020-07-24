10f6540124b8a18fac88892faf3ca509e435de79
Diego Velez <diego.velez@bairesdev.com>
2020-07-24 15:56:34 -0500

    filters for ads working

--- app/assets/javascripts/shared/filter_multimedia.js
+++ app/assets/javascripts/shared/filter_multimedia.js
@@ -1,11 +1,14 @@
+// Create new selector "Contains" which is not case sensitive
+jQuery.expr[':'].Contains = function(a,i,m){
+    return jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase())>=0;
+};
+
 $(document).on('turbolinks:load', function() {
   if ($("#searchMultimedia").length) {
     $("#searchMultimedia").on("input", function() {
-      var value = $(this).val().toLowerCase();
-      $("#multimediaElements .flex-0-f").filter(function() {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-      });
+      var inputText = $(this).val().toLowerCase();
+      $('.mmtitle').closest('.mmcard').hide();
+      $('.mmtitle:Contains("'+ inputText +'")').closest('.mmcard').show();
     });
-
   }
 });
