9272dcfc1f8d123829f5c6f39e79ecf94828ad63
Mauricio Vélez <mauriciovs99@hotmail.com>
2020-07-27 16:21:12 -0500

    added start and end time to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,6 +37,9 @@ if ENV.fetch("RAILS_ENV") != "production"
             board.category = ["television", "billboard", "wallboard"].sample
             board.base_earnings = Faker::Number.between(from: 40000, to: 200000)
             board.social_class = Faker::Number.between(from: 0, to: 3)
+            board.start_time = Time.now
+            board.end_time = Time.now + rand(8).hours
+
           end
         end
       end
