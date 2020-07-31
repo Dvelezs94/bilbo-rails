339ce31942b896d4748d0cbae2bc88a007b36ea9
Pedro Salomón De León Cabello <pitpedrosalo@gmail.com>
2020-07-31 17:20:27 -0500

    emails with region_local rails

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,5 @@ module Bilbo
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = ENV.fetch("RAILS_LOCALE") { :en }.to_sym
-    #config.i18n.available_locales = [:en, :es]
   end
 end
