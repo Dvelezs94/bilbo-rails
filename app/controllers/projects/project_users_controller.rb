77680ed0e7a9e3d649eed1731166832017f201c6
Emiliano Ramos <jesser190698@gmail.com>
2020-08-17 15:23:43 -0500

    traductions

--- app/controllers/projects/project_users_controller.rb
+++ app/controllers/projects/project_users_controller.rb
@@ -8,8 +8,7 @@ class Projects::ProjectUsersController < ApplicationController
     project_user = @current_project.project_users.new(project_user_params)
 
     if project_user.save
-      puts "X" * 400
-      create_notification(recipient_id: @current_project.id, actor_id: @current_project.id, action: "new invite", notifiable: @project, reference: @project_user )
+      create_notification(recipient_id: @current_project.id, actor_id: @current_project.id, action: "new invite", notifiable: @current_project, reference: @project_user)
       flash[:success] = I18n.t('projects.member_invited')
     else
       flash[:error] = I18n.t('projects.member_invited_error')
