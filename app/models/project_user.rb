d5155c61790a43c773bd83c20acb4bbbe9b80c0e
Pedro Salomón De León Cabello <pitpedrosalo@gmail.com>
2020-08-19 13:36:04 -0500

    dont allow providers to be invited to others projects and added translations manage team project

--- app/models/project_user.rb
+++ app/models/project_user.rb
@@ -1,5 +1,6 @@
 class ProjectUser < ApplicationRecord
   attribute :email, :string
+  #before_create :user_invited_provider?
 
   belongs_to :project
   belongs_to :user
@@ -8,8 +9,14 @@ class ProjectUser < ApplicationRecord
 
   before_validation :set_user_id, if: :email?
   validates_uniqueness_of :project_id, :scope => :user_id, :message => "Duplicate"
-
+  validate :user_invited_provider?
   def set_user_id
     self.user = User.invite!(email: email, project_name: email)
+  end
+
+  def user_invited_provider?
+    if self.user.is_provider?
+      return errors.add :base, "Can't invite providers users"
+    end
   end
 end
