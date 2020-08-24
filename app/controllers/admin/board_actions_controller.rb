21a09d784da3cd856a145f7d82f295959aa3ef01
Diego Velez <diego.velez@bairesdev.com>
2020-08-24 17:01:56 -0500

    provider stats

--- app/controllers/admin/board_actions_controller.rb
+++ app/controllers/admin/board_actions_controller.rb
@@ -7,10 +7,15 @@ class Admin::BoardActionsController < ApplicationController
   def provider_statistics
     ##we need to secure this in case the user has more than 1 project in the future
     @project = @user.projects.first
-    @start_date = (params[:select][:year]+"-" +params[:select][:month]+"-"+Date.today.day.to_s).to_datetime.beginning_of_month + 25 rescue Date.today.beginning_of_month + 25
-    @end_date = @start_date.beginning_of_month - 1.month + 24
+    @chosen_month = (params[:select][:year] + "-" + Date::MONTHNAMES[params[:select][:month].to_i]).to_datetime
+    @start_date = @chosen_month - 1.month + 25.days
+    @end_date = @chosen_month + 25.days
+    # end date goes first because we have to check the previous month first
     @monthly_earnings = Board.monthly_earnings_by_board(@project, @start_date..@end_date)
     @monthly_impressions = Board.monthly_impressions(@project, @start_date..@end_date)
+    @earnings = Board.daily_provider_earnings_by_boards(@project, @start_date..@end_date)
+    puts "x" * 500
+    puts @start_date..@end_date
   end
 
 
