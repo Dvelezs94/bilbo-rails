ed8d638688e2b5338571c5f57706bfa0aba68cc8
Diego Velez <diego.velez@bairesdev.com>
2020-08-24 01:32:01 -0500

    default var

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,4 +1,4 @@
-if ENV.fetch("RAILS_ENV") == "production"
+if ENV.fetch("RAILS_ENV") {"development"} == "production"
   Rails.application.config.active_storage.service_urls_expire_in = 1.hour
 else
   Rails.application.config.active_storage.service_urls_expire_in = 7.days
