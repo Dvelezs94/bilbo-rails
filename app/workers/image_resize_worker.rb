326d6325e1bc22f96139ca0e64a0bff280b1c447
Carlos <carloslop0914@gmail.com>
2020-09-02 18:45:44 -0500

    Added front end for the image resizing

--- app/workers/image_resize_worker.rb
+++ app/workers/image_resize_worker.rb
@@ -1,7 +1,6 @@
 class ImageResizeWorker
   include Sidekiq::Worker
   sidekiq_options retry: false, dead: false
-  #require 'mini_magick'
 
   def perform(ad_id,mm_id)
     ad = Ad.find(ad_id)
@@ -20,7 +19,6 @@ class ImageResizeWorker
     image = MiniMagick::Image.open(orig_img_tmpfile.path)
 
     if image.dimensions[1]>1080
-      p image.dimensions
       image.resize "x1080"
     end
 
@@ -29,6 +27,7 @@ class ImageResizeWorker
     ad.multimedia.attach(io: File.open(orig_img_tmpfile), filename: "#{mm.blob.filename.base}."+file_extension, content_type: 'image/'+file_extension)
     delete_img(orig_img_tmpfile,mm)
   end
+
   def delete_img(orig_img_tmpfile,mm)
     orig_img_tmpfile.close
     orig_img_tmpfile.unlink
@@ -36,4 +35,5 @@ class ImageResizeWorker
       mm.destroy!
     end
   end
+
 end
