455ff09acbf5f9b91633de824633f432e0a8f09f
Carlos <carloslop0914@gmail.com>
2020-09-03 18:06:34 -0500

    working on resize images front end

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -5,15 +5,10 @@ class AttachmentsController < ApplicationController
   def create
     if @ad.campaigns.all_off
       @ad.multimedia.attach(params[:files])
-      @name = params[:name]
-      mm = @ad.multimedia.attachments.where(blob_id: ActiveStorage::Blob.where(filename: @name)).last
       #if mm.video?
       #  VideoConverterWorker.perform_async(@ad.id, mm.id)
       #end
-      if mm.image?
-        ImageResizeWorker.perform_async(@ad.id,mm.id)
-      end
-
+      ImageResizeWorker.perform_async(@ad.id)
       flash[:success] = "Attachment saved"
     else
       flash[:error] = I18n.t('ads.errors.wont_be_able_to_update')
--- app/workers/image_resize_worker.rb
+++ app/workers/image_resize_worker.rb
@@ -1,31 +1,36 @@
 class ImageResizeWorker
   include Sidekiq::Worker
-  sidekiq_options retry: false, dead: false
+  include ApplicationHelper
+  sidekiq_options retry: 3, dead: false
 
-  def perform(ad_id,mm_id)
+  def perform(ad_id)
     ad = Ad.find(ad_id)
-    mm = ad.multimedia.find(mm_id)
+    #mm = ad.multimedia.find(mm_id)
 
-    if mm.blob.filename.to_s.ends_with?(".jpg") || mm.blob.filename.to_s.ends_with?(".jpeg")
-      orig_img_tmpfile = Tempfile.new(["#{mm.blob.key}", ".jpeg"])
-      file_extension = "jpeg"
-    elsif mm.blob.filename.to_s.ends_with?(".png")
-      orig_img_tmpfile = Tempfile.new(["#{mm.blob.key}", ".png"])
-      file_extension = "png"
+    ad.images.each do |img|
+
+      meta = get_image_size_from_metadata(img)
+      if meta[:height]>1080 && !img.processed
+        if img.blob.filename.to_s.ends_with?(".jpg") || img.blob.filename.to_s.ends_with?(".jpeg")
+          orig_img_tmpfile = Tempfile.new(["#{img.blob.key}", ".jpeg"])
+          file_extension = "jpeg"
+        elsif img.blob.filename.to_s.ends_with?(".png")
+          orig_img_tmpfile = Tempfile.new(["#{img.blob.key}", ".png"])
+          file_extension = "png"
+        end
+        File.open(orig_img_tmpfile, 'wb') do |f|
+          f.write(img.download)
+        end
+
+        image = MiniMagick::Image.open(orig_img_tmpfile.path)
+        image.resize "x1080"
+        image.write(orig_img_tmpfile.path)
+
+        new_attachment = ad.multimedia.attach(io: File.open(orig_img_tmpfile), filename: "#{img.blob.filename.base}."+file_extension, content_type: 'image/'+file_extension)
+        ad.multimedia.last.update(processed: true)
+        delete_img(orig_img_tmpfile,img)
+      end
     end
-    File.open(orig_img_tmpfile, 'wb') do |f|
-      f.write(mm.download)
-    end
-    image = MiniMagick::Image.open(orig_img_tmpfile.path)
-
-    if image.dimensions[1]>1080
-      image.resize "x1080"
-    end
-
-
-    image.write(orig_img_tmpfile.path)
-    ad.multimedia.attach(io: File.open(orig_img_tmpfile), filename: "#{mm.blob.filename.base}."+file_extension, content_type: 'image/'+file_extension)
-    delete_img(orig_img_tmpfile,mm)
   end
 
   def delete_img(orig_img_tmpfile,mm)
