9e522c761d086dce2047add2d8fb26ecff34ae45
Diego Velez <diego.velez@bairesdev.com>
2020-09-10 16:17:16 -0500

    fix user sig in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,17 +9,4 @@ class SessionsController < Devise::SessionsController
   def destroy
     super
   end
-
-protected
-
-  def after_sign_in_path_for(resource)
-    if resource.is_a?(User) && resource.banned?
-      sign_out resource
-      flash[:error] = "This account has been suspended"
-      root_path
-    else
-      super
-    end
-  end
-
 end
