aa62555ad7878d2bd89a756293e22cfa707aba77
Emiliano Ramos <jesser190698@gmail.com>
2020-09-10 15:39:50 -0500

    banned user sign in validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -15,7 +15,7 @@ class Project < ApplicationRecord
   has_many :reports
   # the project has notifications so all users in the project can see them
   has_many :notifications, foreign_key: :recipient_id
-  after_commit :disable_campaigns!, if: :user_banned?
+  after_commit :disable_campaigns!, on: :update
 
   # useful when you want to retrieve a collection of certain users that have permissions for the project
   # current_user.projects.admins -> will get the admins of all the projects
@@ -33,15 +33,18 @@ class Project < ApplicationRecord
     project_users.where(role: ["owner", "administrator"]).pluck(:user_id)
   end
 
-  def user_banned?
-   if project_users.find_by_role("owner").user.banned?
+  def project_enabled?
+   if self.enabled?
+    true
    else
     false
    end
   end
 
   def disable_campaigns!
+    if owner.banned
       self.campaigns.update_all(state: false)
+    end
   end
   #
   # def users
