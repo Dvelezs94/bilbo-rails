e4817c6cdbb848ce7065efd8a00473e39c9584d2
Pedro Salomón De León Cabello <pitpedrosalo@gmail.com>
2020-09-11 12:34:37 -0500

    MAC address

--- app/controllers/external_sources_controller.rb
+++ app/controllers/external_sources_controller.rb
@@ -6,7 +6,15 @@ class ExternalSourcesController < ApplicationController
 
     def configure
       # make sure the board exists before setting the cookies
-      if Board.find_by(slug: params[:board_slug], access_token: params[:board_token]).present?
+      p "x"*800
+      p params[:mac_address]
+      if params[:mac_address].present?
+        Board.find_by(slug: params[:board_slug], access_token: params[:board_token], mac_address: params[:mac_address]).present?
+          cookies.permanent.signed[:board_slug] = params[:board_slug]
+          cookies.permanent.signed[:board_token] = params[:board_token]
+          cookies.permanent.signed[:mac_address] = params[:mac_address]
+          redirect_to_board
+      elsif Board.find_by(slug: params[:board_slug], access_token: params[:board_token]).present?
         cookies.permanent.signed[:board_slug] = params[:board_slug]
         cookies.permanent.signed[:board_token] = params[:board_token]
         redirect_to_board
@@ -19,15 +27,23 @@ class ExternalSourcesController < ApplicationController
     private
     # if the cookies exist, then redirect to board
     def verify_cookies
-      if cookies.signed[:board_slug] && cookies.signed[:board_token]
-        # even if the cookies are set, we need to make sure the board exists with those parameters
+      if cookies.signed[:board_slug] && cookies.signed[:board_token] && cookies.signed[:mac_address]
         if Board.find_by(slug: cookies.signed[:board_slug], access_token: cookies.signed[:board_token]).present?
+          redirect_to_board
+        end
+      elsif cookies.signed[:board_slug] && cookies.signed[:board_token]
+        # even if the cookies are set, we need to make sure the board exists with those parameters
+        if Board.find_by(slug: cookies.signed[:board_slug], access_token: cookies.signed[:board_token], mac_address: cookies.signed[:mac_address]).present?
           redirect_to_board
         end
       end
     end
 
     def redirect_to_board
-      redirect_to board_path(cookies.signed[:board_slug], access_token: cookies.signed[:board_token], autoplay: true)
+      if cookies.signed[:mac_address]
+      redirect_to board_path(cookies.signed[:board_slug], access_token: cookies.signed[:board_token], mac_address: cookies.signed[:mac_address], autoplay: true)
+    else
+      redirect_to board_path(cookies.signed[:board_slug], access_token: cookies.signed[:board_token], mac_address: cookies.signed[:mac_address], autoplay: true)
     end
+  end
 end
