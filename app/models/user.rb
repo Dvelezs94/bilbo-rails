fa06e9e9becd854efac88be194dc73386ef91dd0
Diego Velez <diego.velez@bairesdev.com>
2020-09-18 15:33:15 -0500

    added facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,24 @@ class User < ApplicationRecord
   attr_readonly :email
   has_many :verifications
 
+  # omniauth functions
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      user.project_name = auth.info.name
+    end
+  end
+
   # returns a hash of total prints per day, WITHOUT grouping the boards
   # def total_board_impressions(start = 4.weeks.ago)
   #   total_impressions = Impression.where(board_id: boards.pluck(:id), created_at: start.beginning_of_day..DateTime.now)
