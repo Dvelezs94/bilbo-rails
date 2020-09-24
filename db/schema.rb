5c00581a4d48d46cbacc975275f28ab01d168104
Diego Velez <diego.velez@bairesdev.com>
2020-09-24 14:38:37 -0500

    ad duration added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_18_201753) do
+ActiveRecord::Schema.define(version: 2020_09_24_193645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_09_18_201753) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.integer "status", default: 0
+    t.integer "duration", default: 10
     t.index ["project_id"], name: "index_ads_on_project_id"
     t.index ["slug"], name: "index_ads_on_slug", unique: true
   end
