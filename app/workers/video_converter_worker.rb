d96ea3bf7c73ccc8040c9c59d20e0681b56a347f
Pedro Salomón De León Cabello <pitpedrosalo@gmail.com>
2020-09-29 12:48:40 -0500

    avi to mp4

--- app/workers/video_converter_worker.rb
+++ app/workers/video_converter_worker.rb
@@ -7,26 +7,23 @@ class VideoConverterWorker
 
     ad.videos.each do |video|
       if !video.processed && video.content_type == "video/x-msvideo" || video.content_type == "video/msvideo" || video.content_type == "video/avi"
-        puts "x" * 500
-        puts video.filename
         orig_video_tmpfile = Tempfile.new(["#{video.blob.key}", ".avi"])
         mp4_video_tmpfile = Tempfile.new(["#{video.blob.key}", ".mp4"])
+
         File.open(orig_video_tmpfile, 'wb') do |f|
           f.write(video.download)
         end
+
         movie = FFMPEG::Movie.new(orig_video_tmpfile.path)
-        p "m" * 800
-        p movie.height
         if movie.height.to_i > 1080
-          p "more than 1920"
           movie.transcode(mp4_video_tmpfile.path, video_encoding_settings)
         else
-          p "nel pastel"
           movie.transcode(mp4_video_tmpfile.path)
         end
+
         ad.multimedia.attach(io: File.open(mp4_video_tmpfile), filename: "#{video.blob.filename.base}.mp4", content_type: 'video/mp4')
         ad.multimedia.last.update(processed: true)
-      delete_video(mp4_video_tmpfile, video ,orig_video_tmpfile)
+        delete_video(mp4_video_tmpfile, video, orig_video_tmpfile)
       else
         video.update(processed: true)
       end
