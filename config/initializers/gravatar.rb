6f3bbc160e4656cd079dfa7c037630d5d643c714
Diego Velez <diego.velez@bairesdev.com>
2020-10-05 18:31:42 -0500

    update gravatar

--- config/initializers/gravatar.rb
+++ config/initializers/gravatar.rb
@@ -1,4 +1,4 @@
 GravatarImageTag.configure do |config|
-  #config.default_image           = nil   # Set this to use your own default gravatar image rather then serving up Gravatar's default image [ 'http://example.com/images/default_gravitar.jpg', :identicon, :monsterid, :wavatar, 404 ].
+  config.default_image            = "https://cdn-bilbo.app.bilbo.mx/statics/default_avatar.png"   # Set this to use your own default gravatar image rather then serving up Gravatar's default image [ 'http://example.com/images/default_gravitar.jpg', :identicon, :monsterid, :wavatar, 404 ].
   config.secure                   = true # Set this to true if you require secure images on your pages.
 end
