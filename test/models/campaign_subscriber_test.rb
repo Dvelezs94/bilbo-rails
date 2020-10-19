a8f6d31870a3f4330c3fa6126e511b1f36da2050
Emiliano Ramos <jesser190698@gmail.com>
2020-10-19 14:24:25 -0500

    test finished

--- test/models/campaign_subscriber_test.rb
+++ test/models/campaign_subscriber_test.rb
@@ -1,18 +1,18 @@
 require 'test_helper'
 
 class CampaignSubscriberTest < ActiveSupport::TestCase
-  #setup do
-  #  @campaign_name = "Campaign subscriber"
-  #  @name = "Subscriber"
-  #  @phone = "+528442467244"
-  #  @project = create(:project, name: @name, status: "enabled")
-  #  @campaign = create(:campaign, name: @campaign_name, project: @project)
-  #  @campaign_subscriber = create(:campaign_subscriber,campaign: @campaign, name: @name , phone: @phone, id: 5)
-  #end
-  #test "has phone" do 
-  #  assert_equal @phone, @campaign_suscriber.last.phone
-  #end
- #test "has campaign" do
-  # assert_equal @campaign_name, @campaign_subscriber.last.name
- #end
+  setup do
+    @campaign_name = "Campaign subscriber"
+    @name = "Subscriber"
+    @phone = "+528442467244"
+    @project = create(:project, name: @name, status: "enabled")
+    @campaign = create(:campaign, name: @campaign_name, project: @project)
+    @campaign_subscriber = create(:campaign_subscriber,campaign: @campaign, name: @name , phone: @phone, id: 5)
+  end
+  test "has phone" do 
+    assert_equal @phone, @campaign.subscribers.last.phone
+  end
+ test "has campaign" do
+   assert_equal @name, @campaign_subscriber.name
+ end
 end
