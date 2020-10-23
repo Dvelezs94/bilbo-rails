c67cf0165c9d489ce535cb86b119430490982c9c
Diego Velez <diego.velez@bairesdev.com>
2020-10-23 10:38:26 -0500

    fix iframe

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -201,7 +201,7 @@ class BoardsController < ApplicationController
   end
 
   def get_boards
-    if current_user.is_provider?
+    if user_signed_in? && current_user.is_provider?
       @boards = @project.boards
     else
       @boards = Board.enabled
