5b7d663378fdb04080a5ae8e254ee19adaac881e
Carlos <carloslop0914@gmail.com>
2020-10-29 17:10:09 -0600

    Update utc offset in a worker instead of job

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,15 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# Configure the geonames api for the timezone gem
+Timezone::Lookup.config(:geonames) do |c|
+  c.username = ENV.fetch("GEONAMES_USER") {"carlos0914"} # <-- username for geonames.org
+  c.offset_etc_zones = true
+end
+#Note: The google api can also be used here, some people use the
+#google api when geonames fails or reaches the credit limit
+
+
 module Bilbo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -19,7 +28,7 @@ module Bilbo
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = ENV.fetch("RAILS_LOCALE") { :en }.to_sym
-    #Set the error pages routes 
+    #Set the error pages routes
     config.exceptions_app = self.routes
   end
 end
