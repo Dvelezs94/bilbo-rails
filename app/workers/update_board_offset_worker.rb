26ecab13dcd4ed9feea675c08db93268207d000d
Carlos <carloslop0914@gmail.com>
2020-10-30 16:12:19 -0600

    working on UpdateBoardOffsetWorker

--- app/workers/update_board_offset_worker.rb
+++ app/workers/update_board_offset_worker.rb
@@ -3,17 +3,20 @@ class UpdateBoardOffsetWorker
   sidekiq_options retry: false, dead: false
 
   def perform
+    # Configure the geonames api for the timezone gem
+    Timezone::Lookup.config(:geonames) do |c|
+      #c.username = ENV.fetch("GEONAMES_USER") {"carlos0914"} # <-- username for geonames.org
+      c.username = 'carlos0914'
+      c.offset_etc_zones = true
+    end
+    #Note: The google api can also be used here, some people use the
+    #google api when geonames fails or reaches the credit limit
     Board.all.each do |b|
-      if b.lat.nil? or b.lng.nil? #Get coordinates of the board in case they aren't already stored
-        p "Latitude or longitude missing for board with id #{b.id}. Updating information..."
-        loc = Geokit::Geocoders::GoogleGeocoder.geocode(b.address)
-        b.update(lat: loc.lat,lng: loc.lng)
-      end
-
       tz = Timezone.lookup(b.lat, b.lng) #Get the timezone of the board by it's coordinates
       offset = tz.utc_offset / 60 #Utc offset given by the Timezone gem is in seconds, so we convert them to minutes
-
-      b.update(utc_offset: offset)
+      if offset != b.utc_offset
+        b.update(utc_offset: offset)
+      end
     end
   end
 end
