d1494d0ccc8f3042aeb6effd1a9566f033b60fe1
Diego Velez <diego.velez@bairesdev.com>
2020-11-09 13:27:32 -0600

    remove project hardcode on tests

--- test/factories/sales.rb
+++ test/factories/sales.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :sale do
-    percent { 1 }
+    percent { 20 }
     starts_at { "2020-11-07 21:12:39" }
     ends_at { "2020-11-07 21:12:39" }
-    description { "MyString" }
+    board_ids { [1,2] }
+    description { "My new Sale" }
   end
 end
