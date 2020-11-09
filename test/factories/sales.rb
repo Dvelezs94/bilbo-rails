0d655c234126660617d1f0b526d360af85b72832
Diego Velez <diego.velez@bairesdev.com>
2020-11-09 16:56:26 -0600

    sales test

--- test/factories/sales.rb
+++ test/factories/sales.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :sale do
     percent { 20 }
     starts_at { "2020-11-07 21:12:39" }
-    ends_at { "2020-11-07 21:12:39" }
+    ends_at { "2020-11-08 21:12:39" }
     board_ids { [1,2] }
     description { "My new Sale" }
   end
