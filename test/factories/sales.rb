df7f7369d40269501ea1d8613288f712391a8924
Diego Velez <diego.velez@bairesdev.com>
2020-11-09 08:53:41 -0600

    add sales to bilbo

---
+++ test/factories/sales.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :sale do
+    percent { 1 }
+    starts_at { "2020-11-07 21:12:39" }
+    ends_at { "2020-11-07 21:12:39" }
+    description { "MyString" }
+  end
+end
