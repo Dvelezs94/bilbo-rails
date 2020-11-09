8ab35acf09b687961c3797b64ca480835d13df42
Diego Velez <diego.velez@bairesdev.com>
2020-11-09 13:27:32 -0600

    remove project hardcode on tests

--- test/models/sale_test.rb
+++ test/models/sale_test.rb
@@ -1,7 +1,11 @@
-require 'test_helper'
-
-class SaleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+# require 'test_helper'
+#
+# class SaleTest < ActiveSupport::TestCase
+#   setup do
+#     @name = "Project Sales"
+#     @project =  create(:project, name: @name)
+#     @user = create(:user,role: "provider", name: @name)
+#     @campaign = create(:campaign, name: @campaign_name,project: @user.projects.first, project_id: @project_id)
+#   end
+#
+# end
