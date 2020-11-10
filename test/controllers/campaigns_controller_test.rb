2bbc3a48e042c13baeaece58866a568e08d36bab
Diego Velez <diego.velez@bairesdev.com>
2020-11-10 12:50:28 -0600

    remove hardcoded values on test

--- test/controllers/campaigns_controller_test.rb
+++ test/controllers/campaigns_controller_test.rb
@@ -4,9 +4,8 @@ class CampaignsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @campaign_name = "Zoro"
     @user = create(:user, name: @campaign_name )
-    @board_id = "2"
     @board = create(:board,project: @user.projects.first, name: "LUFFY", lat: "180558", lng: "18093", avg_daily_views: "800000", width: "1280", height: "720",
-      address: "mineria 908", category: "A", base_earnings: "5000", face: "north", id: @board_id)
+      address: "mineria 908", category: "A", base_earnings: "5000", face: "north")
     @project =  create(:project, name: @campaign_name)
     @campaign = create(:campaign, name: @campaign_name,project: @user.projects.first, project_id: @project.id)
     sign_in @user
