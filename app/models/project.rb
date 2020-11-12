08d76380da9c31d41d21edbf6ed9d5b28d624780
Diego Velez <diego.velez@bairesdev.com>
2020-11-12 14:33:19 -0600

    notif for providers if project owner has credits

--- app/models/project.rb
+++ app/models/project.rb
@@ -87,9 +87,13 @@ class Project < ApplicationRecord
   # campaigns that require provider feedback to be aither approved or denied
   def campaigns_for_review
     Campaign.active.where.not(ad_id: nil).joins(:boards).merge(self.boards).pluck(:id).each do |c|
+      @campaign_loop = Campaign.find(c)
       #Search for ads that haven't been processed
-       if Ad.find(Campaign.find(c).ad_id).processed?
-          @camp = Array(@camp).push(c)
+       if Ad.find(@campaign_loop.ad_id).processed?
+         # to be optimized
+         if @campaign_loop.owner.has_had_credits?
+           @camp = Array(@camp).push(c)
+         end
        end
      end
     BoardsCampaigns.where(board: self.boards.enabled.pluck(:id), campaign: @camp).in_review.count
