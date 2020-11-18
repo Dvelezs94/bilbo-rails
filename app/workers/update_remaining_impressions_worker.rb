6d36acc5bce1ab35f61322461c57749e1c1e3e9f
Mauricio Vélez <mauriciovs99@hotmail.com>
2020-11-18 15:04:17 -0600

    modified worker

--- app/workers/update_remaining_impressions_worker.rb
+++ app/workers/update_remaining_impressions_worker.rb
@@ -3,15 +3,16 @@ class UpdateRemainingImpressionsWorker
   sidekiq_options retry: false, dead: false
 
   def perform
-    Campaign.where(clasification: "budget").each do |cpn|
-      cpn.boards.each do |b|
-        imp = (cpn.budget_per_bilbo/b.get_cycle_price(cpn)).to_i
-        bc = BoardsCampaigns.find_by(campaign: cpn, board: b)
-        if bc.remaining_impressions != imp
-          bc.update(remaining_impressions: imp)
-        end
-      end
-    end
+    BoardsCampaigns.includes(:campaign).all.update(update_remaining_impressions: true)
+    # Campaign.where(clasification: "budget").each do |cpn|
+    #   cpn.boards.each do |b|
+    #     imp = (cpn.budget_per_bilbo/b.get_cycle_price(cpn)).to_i
+    #     bc = BoardsCampaigns.find_by(campaign: cpn, board: b)
+    #     if bc.remaining_impressions != imp
+    #       bc.update(update_remaining_impressions: true)
+    #     end
+    #   end
+    # end
     p "All user remaining impressions have been reseted"
   end
 end
