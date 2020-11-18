6e5e45975a34fae9f8f509c89a2899ddd8fb22d8
Mauricio Vélez <mauriciovs99@hotmail.com>
2020-11-18 15:14:50 -0600

    modified worker

--- app/workers/update_remaining_impressions_worker.rb
+++ app/workers/update_remaining_impressions_worker.rb
@@ -5,12 +5,13 @@ class UpdateRemainingImpressionsWorker
   def perform
     BoardsCampaigns.includes(:campaign).all.update(update_remaining_impressions: true)
     Board.all.each do |board|
-    ActionCable.server.broadcast(
-      board.slug,
-      action: "update_rotation",
-      ads_rotation: board.add_bilbo_campaigns.to_s,
-      remaining_impressions: board.get_user_remaining_impressions.to_s
-    )
+      ActionCable.server.broadcast(
+        board.slug,
+        action: "update_rotation",
+        ads_rotation: board.add_bilbo_campaigns.to_s,
+        remaining_impressions: board.get_user_remaining_impressions.to_s
+      )
+    end
     p "All user remaining impressions have been reseted"
   end
 end
