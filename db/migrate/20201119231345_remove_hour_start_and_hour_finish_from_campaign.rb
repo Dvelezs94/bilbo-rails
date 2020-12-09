dc376e336c4c698ca56aeb7995fdb6a371f7c7de
Mauricio Vélez <mauriciovs99@hotmail.com>
2020-12-09 15:57:37 -0600

    changed name of migration

--- db/migrate/20201119231345_remove_hour_start_and_hour_finish_from_campaign.rb
+++ db/migrate/20201119231345_remove_hour_start_and_hour_finish_from_campaign.rb
@@ -1,4 +1,4 @@
-class RemoveHourStartandHourFinishFromCampaign < ActiveRecord::Migration[6.0]
+class RemoveHourStartAndHourFinishFromCampaign < ActiveRecord::Migration[6.0]
   def change
     remove_column :campaigns, :hour_start, :time
     remove_column :campaigns, :hour_finish, :time
