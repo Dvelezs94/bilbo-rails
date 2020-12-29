edf1379b74632fff6e44b808e29a9210270e6671
Pedro Salomón De León Cabello <pitpedrosalo@gmail.com>
2020-12-29 11:41:38 -0600

    ability to copy campaign

--- config/routes.rb
+++ config/routes.rb
@@ -48,6 +48,7 @@ Rails.application.routes.draw do
       get :getAds
       get :get_used_boards
       get :copy_campaign
+      post :create_copy
     end
     collection do
       get :provider_index
