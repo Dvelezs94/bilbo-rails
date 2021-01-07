dc5f02527990d5c71e810b484bcb59389c75a059
Diego Velez <diego.velez@bairesdev.com>
2021-01-07 16:13:21 -0600

    fix tests

--- test/factories/campaign.rb
+++ test/factories/campaign.rb
@@ -2,10 +2,11 @@ FactoryBot.define do
     factory :campaign do
       name { Faker::Name.name }
       description { "He is one of the main combat specialists of the Straw Hat Pirates".downcase }
-      budget  {Faker::Number.between(from: 5, to: 50)}
+      budget  {Faker::Number.between(from: 50, to: 200)}
       state   {Faker::Boolean.boolean}
       status  {Faker::Number.between(from: 0, to: 1)}
       boards  {Board.order('RANDOM()').first(Faker::Number.between(from: 2, to: 7))}
+      objective { 0 }
       project {}
       slug {}
       provider_campaign {false}
