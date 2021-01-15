f0bd613b086d88c31854a12194db5c1647c62dfd
Pedro Salomón De León Cabello <pitpedrosalo@gmail.com>
2021-01-15 14:07:02 -0600

    innit

--- app/helpers/sendgrid_helper.rb
+++ app/helpers/sendgrid_helper.rb
@@ -3,6 +3,44 @@
 module SendgridHelper
 
   def sync_sendgrid_user(user)
+    p "x" * 800
+    names = user.name.split(" ")
+    if names.length == 1
+      @name = names[0].to_s
+      @last_name = "string (optional)"
+    elsif names.length == 2
+      @name = names[0]
+      @last_name = names[1]
+    elsif names.length == 3
+      @name = names[0]
+      @last_name = names[1] + " " + names[2]
+    elsif names.length == 4
+      @name = names[0] + " " + names[1]
+      @last_name = names[2] + " " + names[3]
+    else
+      i = 0
+      p ";;"
+      while i < names.length
+        p names.include? "De"
+
+
+        if names.include? "De"
+          o = 0
+          while o < names.find_index("De")
+            @aux = names[i] + " "
+            @names += @aux
+            o += 1
+          end
+          while o >= names.find_index("De")
+            @last_name += names[i] + " "
+          end
+        end
+        i += 1
+      end
+    end
+    p @name
+    p @last_name
+
     if Rails.env.production?
       require 'uri'
       require 'net/http'
@@ -15,6 +53,7 @@ module SendgridHelper
       else
         list_id = "4e7b5aeb-302c-465e-b667-13dbae1d7bd7"
       end
+
       request = Net::HTTP::Put.new(url)
       request["authorization"] = "#{SendGrid::API.new(api_key: ENV["SENDGRID_API_KEY"]).request_headers.first[1]}"
       request["content-type"] = 'application/json'
