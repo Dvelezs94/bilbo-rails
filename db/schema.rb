bef012b10da129519ba576468c2952ae7520c1ea
Pedro Salomón De León Cabello <pitpedrosalo@gmail.com>
2021-01-27 12:12:06 -0600

    added impression_count

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_08_200245) do
+ActiveRecord::Schema.define(version: 2021_01_25_210810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -211,6 +211,7 @@ ActiveRecord::Schema.define(version: 2020_12_08_200245) do
     t.string "analytics_token"
     t.string "link"
     t.integer "objective", default: 0
+    t.integer "impression_count", default: 0
     t.index ["ad_id"], name: "index_campaigns_on_ad_id"
     t.index ["project_id"], name: "index_campaigns_on_project_id"
     t.index ["slug"], name: "index_campaigns_on_slug", unique: true
@@ -360,7 +361,7 @@ ActiveRecord::Schema.define(version: 2020_12_08_200245) do
   create_table "shorteners", force: :cascade do |t|
     t.string "target_url"
     t.string "token"
-    t.datetime "expires_at", default: "2031-01-11 23:39:06"
+    t.datetime "expires_at", default: "2031-01-25 21:00:16"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "qr"
@@ -468,4 +469,7 @@ ActiveRecord::Schema.define(version: 2020_12_08_200245) do
   add_foreign_key "provider_invoices", "users"
   add_foreign_key "reports", "boards"
   add_foreign_key "reports", "campaigns"
+  add_foreign_key "reports", "projects"
+  add_foreign_key "user_activities", "users"
+  add_foreign_key "verifications", "users"
 end
