7534b40427a0be1d75736ab3464b3b8013707a20
Carlos Lopez <carloslop0914@gmail.com>
2021-02-05 23:14:06 +0000

    Delete duplicated impressions fix

--- db/schema.rb
+++ db/schema.rb
@@ -363,7 +363,7 @@ ActiveRecord::Schema.define(version: 2021_01_27_231134) do
   create_table "shorteners", force: :cascade do |t|
     t.string "target_url"
     t.string "token"
-    t.datetime "expires_at", default: "2031-01-12 19:00:57"
+    t.datetime "expires_at", default: "2031-01-30 01:36:31"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "qr"
