424f61be0f76ec795eb25eff0d56e92771aea9aa
carloslop0914 <carloslop0914@gmail.com>
2021-03-24 18:04:30 -0600

    Added restrictions to boards, change board name value according to csv data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_235527) do
+ActiveRecord::Schema.define(version: 2021_03_24_225620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -157,6 +157,7 @@ ActiveRecord::Schema.define(version: 2021_03_22_235527) do
     t.integer "displays_number", default: 1
     t.datetime "ads_rotation_updated_at"
     t.boolean "external", default: false
+    t.string "restrictions"
     t.index ["project_id"], name: "index_boards_on_project_id"
     t.index ["slug"], name: "index_boards_on_slug", unique: true
   end
