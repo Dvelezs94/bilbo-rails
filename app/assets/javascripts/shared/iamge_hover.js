4181072f3aa70bb588432914710ec034c13be187
pedro <pitpedrosalo@gmail.com>
2021-03-26 11:18:33 -0600

    added hover to delete contents

--- app/assets/javascripts/shared/iamge_hover.js
+++ app/assets/javascripts/shared/iamge_hover.js
@@ -1,7 +1,11 @@
 $(document).on('turbolinks:load', function() {
+  if($('.img-caption').length){
+    hover_img();
+  }
+});
 
+function hover_img(){
     $('.img-caption').on('mouseover mouseout', function(){
       $(this).find('figcaption').toggleClass('op-0');
     });
-
-});
+}
