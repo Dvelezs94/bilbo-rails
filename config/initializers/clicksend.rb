754929d314aef3863a2f68fc77ede32dea9a19c1
github.com-oh <diego.sanchez@outcomehealth.com>
2021-03-31 17:33:33 -0600

    fix clicksend stuff

--- config/initializers/clicksend.rb
+++ config/initializers/clicksend.rb
@@ -1,8 +1,8 @@
 require 'clicksend'
 
 # put your own credentials here
-username = ENV.fetch('CLICKSEND_USERNAME')      # Your ClickSend username.
-api_key = ENV.fetch('CLICKSEND_API_KEY')    # Your Secure Unique API key.
+username = ENV.fetch('CLICKSEND_USERNAME') {""}     # Your ClickSend username.
+api_key = ENV.fetch('CLICKSEND_API_KEY') {""}   # Your Secure Unique API key.
 
 # set up a client to talk to the ClickSend REST API
 CLICKSEND_CLIENT = ClickSend::REST::Client.new(:username => username, :api_key => api_key)
