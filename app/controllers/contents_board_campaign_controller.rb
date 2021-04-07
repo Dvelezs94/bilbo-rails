e98edd2a0eebb2ccf58075083ba0c52d82fea64c
pedro <pitpedrosalo@gmail.com>
2021-04-07 16:55:37 -0500

    changed color for select and fixed button add content transparent

--- app/controllers/contents_board_campaign_controller.rb
+++ app/controllers/contents_board_campaign_controller.rb
@@ -1,20 +1,22 @@
 class ContentsBoardCampaignController < ApplicationController
   def multimedia
     #Return the content for modal step 2 for show bilbos
-    @accept_only_images = Board.find_by_slug(params[:board_slug]).images_only
+    board = Board.friendly.find(params[:board_slug])
+    @accept_only_images = board.images_only
+    @campaign = Campaign.find(params[:campaign])
     if @accept_only_images
       @content = []
-      Campaign.find(params[:campaign]).project.contents.each do |content|
+      @campaign.project.contents.each do |content|
         if !content.is_video?
           @content.push(content)
         end
       end
     else
-      @content = Campaign.find(params[:campaign]).project.contents.map{|content| content}
+      @content = Campaign.find(params[:campaign]).project.contents.map{|content|content}
     end
     @board_name = params[:board_name]
     @slug = params[:board_slug]
-    render  'campaigns/wizard/multimedia', :locals => {:content => @content, :board_name => @board_name, :slug => @slug, :accept_only_images => @accept_only_images}
+    render  'campaigns/wizard/multimedia', :locals => {:content => @content, :board_name => @board_name, :slug => @slug, :accept_only_images => @accept_only_images, :campaign => @campaign}
   end
 
   def get_content
@@ -22,6 +24,7 @@ class ContentsBoardCampaignController < ApplicationController
     @selected_contents = Content.where(id: params[:selected_contents].split(" "))
     @board_slug = params[:board_slug]
     @board = Board.friendly.find(params[:board_slug])
-    render  'campaigns/wizard/get_content', :locals => {:selected_content => @selected_contents, :board_slug => @board_slug}
+    @campaign = params[:campaign]
+    render  'campaigns/wizard/get_content', :locals => {:selected_content => @selected_contents, :board => @board, :campaign => @campaign}
   end
 end
