f1b07550e9033576d53cd2edf9d8907339451a51
pedro <pitpedrosalo@gmail.com>
2021-04-07 16:55:37 -0500

    changed color for select and fixed button add content transparent

--- app/assets/javascripts/shared/select_ad.js
+++ app/assets/javascripts/shared/select_ad.js
@@ -32,25 +32,30 @@ function select_ad() {
 
 function select_content() {
   // choose ad in wizard
-  if ($('.glightbox').length) {
-    $('.glightbox').click(function(e) {
+  if ($('.card.wd-30p').length) {
+    $('.card.wd-30p').click(function(e) {
       e.preventDefault();
-      //$('.wizard_selected_ad').removeClass('wizard_selected_ad');
-      $(this).find('#card-ad-link').addClass('wizard_selected_ad');
-      //$('#campaign_ad_id').val($(this).attr('id'));
+      if ($(this).hasClass('wizard_selected_ad_blue')){
+        $(this).removeClass('wizard_selected_ad_blue');
+      }else {
+      $(this).addClass('wizard_selected_ad_blue');  
+      }
 
     });
 
     if ($('#'+$('#slug-board').val()).val().split(" ").length) {
       console.log("Slug: " + $("#slug-board").val());
       selected_contents_ids = $('#'+$('#slug-board').val()).val().split(" ");
+      console.log("select_contents: "+ selected_contents_ids);
       //var selected_ad = $('#' + selected_ad_id);
       var i;
       for (i = 0; i < selected_contents_ids.length; i++) {
         selected_ad = selected_contents_ids[i]
-        select_content_mark = $(document.querySelectorAll('[data-id='+ "'"+selected_ad+"'" + ']'))
+        console.log("selected ad: " + selected_ad);
+        select_content_mark = $(document.querySelectorAll('[data-content='+ "'"+selected_ad+"'" + ']'))
         console.log(select_content_mark)
-        select_content_mark.find('#card-ad-link').addClass('wizard_selected_ad');
+        console.log("find: " + select_content_mark.find('.card.wd-30p'))
+        select_content_mark.addClass('wizard_selected_ad_blue');
       }
 
     }
