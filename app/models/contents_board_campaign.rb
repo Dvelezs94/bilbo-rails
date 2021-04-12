8011d7a936bd7aa233444126a9d55ca50d11c07e
github.com-oh <diego.sanchez@outcomehealth.com>
2021-04-12 13:57:33 -0500

    in revision fixed

--- app/models/contents_board_campaign.rb
+++ app/models/contents_board_campaign.rb
@@ -1,4 +1,14 @@
 class ContentsBoardCampaign < ApplicationRecord
   belongs_to :content
   belongs_to :boards_campaigns
+
+  after_create :set_board_campaigns_to_review
+  before_destroy :set_board_campaigns_to_review
+
+  private
+
+  def set_board_campaigns_to_review
+    puts "x" * 500
+    self.boards_campaigns.update_column(:status, BoardsCampaigns.statuses[:in_review])
+  end
 end
