9a35e1a30dc36a958da924c90c48148f71cae9c6
github.com-oh <diego.sanchez@outcomehealth.com>
2021-04-13 11:27:07 -0500

    content controller test workig

---
+++ test/factories/content.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :content do
+    url { "https://bilbo.mx" }
+    project {}
+    duration {}
+    multimedia_data {}
+  end
+end
--- test/factories/contents.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :content do
-    slug { "MyString" }
-    url { "MyString" }
-    project_id { nil }
-  end
-end
