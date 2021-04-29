c8983473f840864369d0ffad269a5ec3eedbf106
github.com-oh <diego.sanchez@outcomehealth.com>
2021-04-28 23:43:17 -0500

    initial commit

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -223,6 +223,7 @@ class BoardsController < ApplicationController
                                   :mac_address,
                                   :keep_old_cycle_price_on_active_campaigns,
                                   :displays_number,
+                                  :provider_price,
                                   :restrictions,
                                   images: [],
                                   default_images: []
