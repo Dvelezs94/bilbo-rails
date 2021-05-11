7e25b10a96a27c403fcf7003ff191457770ab3ea
github.com-oh <diego.sanchez@outcomehealth.com>
2021-05-11 12:08:55 -0500

    auto refresh working

--- app/assets/javascripts/shared/content_functions.js
+++ app/assets/javascripts/shared/content_functions.js
@@ -1,7 +1,5 @@
-function get_single_content(){
-  const elem = $(event.target);
-  const card_elem = elem.closest(".card");
-  const content_id = card_elem.data("content");
+function get_single_content(content_id){
+  elem = $("#reload_single_content_" + content_id);
   $.ajax({
     url:  "/contents_board_campaign/"+ content_id +"/fetch_single_wizard_content",
     beforeSend: function () {
@@ -15,3 +13,16 @@ function get_single_content(){
     }
   });
 };
+
+// automatic refreshes content every 5 seconds for the uploaded content
+function auto_fetch_content(content_id) {
+  console.log("attempting to auto fetch content for: " + content_id)
+  var tid = setInterval(function() {
+    div_elem = $("#reload_single_content_" + content_id);
+    if (div_elem.length) { // if element hasn't been processed yet, so keep trying
+      get_single_content(content_id);
+    } else { // the element was processed correctly, so no need to keep running
+      clearInterval(tid);
+    }
+  }, 5000);
+}
