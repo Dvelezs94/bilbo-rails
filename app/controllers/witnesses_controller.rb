4c974539235a928865124c21948d4399478f24da
PinguiPit <pitpedrosalo@gmail.com>
2021-05-26 12:41:01 -0500

    fixed witness test

--- app/controllers/witnesses_controller.rb
+++ app/controllers/witnesses_controller.rb
@@ -1,8 +1,9 @@
 class WitnessesController < ApplicationController
+  access [:user, :provider] => :all
+  before_action :validate_user_present, only:[:get_campaign, :create, :validate_weekly_generation]
   before_action :get_campaign, only:[:create, :validate_weekly_generation]
   before_action :validate_weekly_generation, only:[:create]
   before_action :set_witness, only: [:evidences_witness_modal, :evidences_dashboard_provider]
-  access all: [:create], user: :all
 
   def create
     if @campaign.state
@@ -33,6 +34,7 @@ class WitnessesController < ApplicationController
     render 'evidences/evidences_dashboard'
   end
 
+  private
   def validate_weekly_generation
     if @campaign.witnesses.present?
       @campaign.witnesses.where(created_at: 1.day.ago..Time.zone.now).exists?
@@ -40,10 +42,13 @@ class WitnessesController < ApplicationController
       redirect_to  analytics_campaign_path(@campaign.slug)
     end
   end
-  private
+
+  def validate_user_present
+    redirect_to user_session_path if !current_user.present?
+  end
 
   def get_campaign
-    @campaign= @project.campaigns.friendly.find(witness_params[:campaign_id])
+    @campaign = @project.campaigns.friendly.find(witness_params[:campaign_id])
   end
 
   def get_all_witnesses
