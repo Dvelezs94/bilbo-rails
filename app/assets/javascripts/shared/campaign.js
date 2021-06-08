9513f07afcf4e9c8e03cc2127f579471053c9b93
oh-diego-velez <diego.sanchez@outcomehealth.com>
2021-06-08 15:38:05 -0500

    copy campaign working fine

--- app/assets/javascripts/shared/campaign.js
+++ app/assets/javascripts/shared/campaign.js
@@ -1,4 +1,15 @@
-$(document).on('turbolinks:load', function() {
+function initDatePicker() {
+  $("#campaign_starts_at").datepicker({
+    minDate: 0,
+    dateFormat: 'yy-mm-dd'
+  });
+  $("#campaign_ends_at").datepicker({
+    minDate: 1,
+    dateFormat: 'yy-mm-dd'
+  });
+}
+
+function newCampaignOptions() {
   if($("#budget_card").length){
     $('.card-ad-link').click(function (e) {
       $("#budget_card").removeClass('wizard_selected_ad');
@@ -10,14 +21,21 @@ $(document).on('turbolinks:load', function() {
     $("#budget_card").addClass('wizard_selected_ad');
 
   }
-  if($("#campaign_starts_at").length){
-    $("#campaign_starts_at").datepicker({
-      minDate: 0,
-      dateFormat: 'yy-mm-dd'
-    })
-    $("#campaign_ends_at").datepicker({
-      minDate: 1,
-      dateFormat: 'yy-mm-dd'
-    })
-  }
-});
+
+  $("[id^=objective_]").click(function(e){
+    var objective_id = $(this).attr('data-objective');
+    console.log(objective_id);
+    $("[id^=objective_]").removeClass("selected_card");
+    $(this).addClass('selected_card');
+    $("#campaign_objective").val(objective_id);
+    if (objective_id === "interaction") {
+      $("#campaign_link_form").removeClass("d-none");
+      $("#campaign_link").attr("required", true);
+    } else {
+      $("#campaign_link_form").addClass("d-none");
+      $("#campaign_link").attr("required", false);
+    }
+  });
+  
+  initDatePicker();
+}
