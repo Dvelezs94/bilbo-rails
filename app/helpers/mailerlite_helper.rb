869b179f9365483326d6bc153bae4f45f3b8c0bb
oh-diego-velez <diego.sanchez@outcomehealth.com>
2021-06-25 12:18:27 -0500

    fix mailerlite

--- app/helpers/mailerlite_helper.rb
+++ app/helpers/mailerlite_helper.rb
@@ -14,5 +14,29 @@ module MailerliteHelper
         return result
       end
     end
+
+    def sync_all
+      user_group_id = 107759053
+      provider_group_id = 107759071
+
+
+      users = User.where(roles: "user")
+      providers = User.where(roles: "provider")
+
+      users_subscribers = []
+      providers_subscribers = []
+
+      for user in users
+        users_subscribers.push({ email: user.email, name: user.name_or_none })
+      end
+
+      for provider in providers
+        providers_subscribers.push({ email: provider.email, name: provider.name_or_none })
+      end
+
+      # Call methods to create subscribers
+      MailerLite.import_group_subscribers(user_group_id, users_subscribers, resubscribe: false)
+      MailerLite.import_group_subscribers(provider_group_id, providers_subscribers, resubscribe: false)
+    end
   end
   
