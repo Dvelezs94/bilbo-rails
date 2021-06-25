d553f3bcb7cc327c35c473d11b9b9e45c3036b5b
oh-diego-velez <diego.sanchez@outcomehealth.com>
2021-06-25 12:18:27 -0500

    fix mailerlite

--- app/workers/sync_email_contacts_worker.rb
+++ app/workers/sync_email_contacts_worker.rb
@@ -5,13 +5,6 @@ class SyncEmailContactsWorker
   sidekiq_options retry: false, dead: false
 
   def perform
-    User.where.not(roles: "admin").each do |user|
-      puts user.email
-      begin
-        sync_mailerlite_user(user)
-      rescue MailerLite::BadRequest => e
-        Bugsnag.notify("Mailerlite error for user #{user.email}: #{e}")
-      end
-    end
+    MailerliteHelper.sync_all()
   end
 end
