4a853306c1acceb1125f29bffd495210900de511
Carlos0914 <carloslop0914@gmail.com>
2021-07-26 11:22:21 -0500

    Added tests for board occupation

--- test/controllers/ads_rotation_test.rb
+++ test/controllers/ads_rotation_test.rb
@@ -344,13 +344,4 @@ class AdsRotationTest < ActionDispatch::IntegrationTest
   end
 
   ########################################### End Remaining impressions test ###############################
-  private
-
-  def rand_time(from, to=Time.now.utc)
-    Time.at(rand_in_range(from.to_f, to.to_f))
-  end
-
-  def rand_in_range(from, to)
-    rand * (to - from) + from
-  end
 end
