7299c0d66057f1825524739e44af3be23f1b8383
Carlos0914 <carloslop0914@gmail.com>
2021-07-26 15:46:24 -0500

    Bug Fix on occupation calculation for hour campaigns

--- db/schema.rb
+++ db/schema.rb
@@ -421,7 +421,7 @@ ActiveRecord::Schema.define(version: 2021_07_20_145934) do
   create_table "shorteners", force: :cascade do |t|
     t.string "target_url"
     t.string "token"
-    t.datetime "expires_at", default: "2031-07-19 22:43:38"
+    t.datetime "expires_at", default: "2031-07-09 16:06:21"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "qr"
