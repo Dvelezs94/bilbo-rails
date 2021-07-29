b522fc20b2d344ef140efe4d8517937b22a8b2b5
Mauricio Velez <mauriciovelsa99@gmail.com>
2021-07-29 13:30:05 -0500

    added filters to map view

--- app/assets/javascripts/shared/filter/submit_on_change.js
+++ app/assets/javascripts/shared/filter/submit_on_change.js
@@ -1,5 +1,7 @@
 $(document).on('turbolinks:load', function() {
   $('form#filter-bilbos.send-on-change').change(function(event) {
+    //dont send when changes ionslider
+    if (event.target.id == "price_slider") return(0);
     //dont send form when location changes and not select one
     if (event.target.id != "search_autocomplete") $(this).find("input[type=submit]").click();
   });
