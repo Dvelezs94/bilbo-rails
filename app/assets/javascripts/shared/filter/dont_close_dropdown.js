985236ec1df829940010991c6c293f47c829b539
Mauricio Velez <mauriciovelsa99@gmail.com>
2021-08-04 11:30:08 -0500

    filter-bilbos has the problem that has to be inside edit campaigns form, so now its outside but its filled dynamically on event change of div

--- app/assets/javascripts/shared/filter/dont_close_dropdown.js
+++ app/assets/javascripts/shared/filter/dont_close_dropdown.js
@@ -1,5 +1,5 @@
 $(document).on('turbolinks:load', function() {
-  $('.dont-close-dropdown, #filter-bilbos input[type="submit"]').on('click', function (e) {
+  $(document).on('click', '.dont-close-dropdown, #filter-bilbos input[type="submit"]', function(e){
     //first element: prevents dropdown from closing when clicking inside it
     //second element: when js clicks submit (ocurrs when something changes in the filter), it closes dropdowns because its outside them, so i added it to stop propagation
     e.stopPropagation();
