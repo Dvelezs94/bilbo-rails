f58423bf16ef5e195fce34b827c0746788dc9443
PinguiPit <pitpedrosalo@gmail.com>
2021-08-05 12:04:22 -0500

    Dashboard_player

--- app/controllers/dashboard_players_controller.rb
+++ app/controllers/dashboard_players_controller.rb
@@ -14,17 +14,17 @@ class DashboardPlayersController < ApplicationController
        @dashboard_player = DashboardPlayer.create(dashboard_player_params)
       if @dashboard_player.save
         if @dashboard_player_board = @dashboard_player.board_dashboard_players.where(board_id: Board.friendly.find(dashboard_player_params[:board_slug]).id).first_or_create
-          @success_message = I18n.t("ads.transitions.update.success")
+          @success_message = I18n.t("dashboard_player.add_bilbo")
           @player= @project.dashboard_player
         else
-          @error_message = I18n.t("ads.transitions.update.error")
+          @error_message = I18n.t("dashboard_player.error_player")
         end
       end
     else
       if @dashboard_player_board = @project.dashboard_player.board_dashboard_players.where(board_id: Board.friendly.find(dashboard_player_params[:board_slug]).id).first_or_create
-        @success_message = I18n.t("ads.transitions.update.success")
+        @success_message = I18n.t("dashboard_player.add_bilbo")
       else
-        @error_message = I18n.t("ads.transitions.update.error")
+        @error_message = I18n.t("dashboard_player.error_player")
       end
     end
     render 'dashboard_players/add_player'
@@ -33,9 +33,9 @@ class DashboardPlayersController < ApplicationController
   def delete_player
     @board = Board.friendly.find(params[:board_id_dashboard_player])
     if @project.dashboard_player.board_dashboard_players.find_by(board_id: @board.id).destroy
-      @success_message = I18n.t("ads.transitions.update.success")
+      @success_message = I18n.t("dashboard_player.remove_bilbo")
     else 
-      @error_message = I18n.t("ads.transitions.update.error")
+      @error_message = I18n.t("dashboard_player.error_remove")
     end
   end
 
@@ -48,7 +48,7 @@ class DashboardPlayersController < ApplicationController
       if @project.dashboard_player.present?
         @project.dashboard_player.board_dashboard_players.map{|player| p player.board.slug}
         if @project.dashboard_player.board_dashboard_players.map{|player| player.board.slug}.include? dashboard_player_params[:board_slug].to_s
-          @error_message = I18n.t("ads.transitions.update.error")
+          @error_message = I18n.t("dashboard_player.transitions.update.error")
           render 'dashboard_players/message'
         end
       end
