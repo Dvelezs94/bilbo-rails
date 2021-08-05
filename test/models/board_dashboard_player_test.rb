4c5e90b4844298095ae113ff5421c20a70a8b296
PinguiPit <pitpedrosalo@gmail.com>
2021-08-05 13:50:44 -0500

    Dashboard player

--- test/models/board_dashboard_player_test.rb
+++ test/models/board_dashboard_player_test.rb
@@ -5,14 +5,11 @@ class BoardDashboardPlayerTest < ActiveSupport::TestCase
     @name = "Dashboard Player"
     @user = create(:user, name: @name, roles: "provider")
     @project = @user.projects.first
-    @dashboard_player = create(:dashboard_player, project: @user.projects.first) 
+    @dashboard_player = create(:dashboard_player, project_id: @project.id) 
     @board = create(:board,project: @user.projects.first, name: "Board", lat: "180558", lng: "18093", avg_daily_views: "800000", width: "1280", height: "720", address: "mineria 908", category: "A", base_earnings: "5000", provider_earnings: "4000", face: "north")
-    @board_dashboard_players = create(:board_dashboard_player, dashboard_player_id: @dashboard_player.id, board_id: @board.id )
+    @board_dashboard_player = create(:board_dashboard_player, dashboard_player_id: @dashboard_player.id, board_id: @board.id  ) 
   end
-  test "create player" do
-    assert_equal true, @board_dashboard_players.present?
-    sign_in @user
-    @image_attachment = fixture_file_upload('test_image.png','image/png')
-    post create_multimedia_contents_url, params: { multimedia: @image_attachment }, xhr: true
+  test "create board dashboard player" do
+    assert_equal true, @project.dashboard_player.board_dashboard_players.present?
    end
 end
