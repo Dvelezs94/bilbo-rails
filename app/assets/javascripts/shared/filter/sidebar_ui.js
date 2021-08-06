d3241f82b8e7bc4a4d0eef8fb52283450702ba1f
Mauricio Velez <mauriciovelsa99@gmail.com>
2021-08-06 12:34:04 -0500

    added transition effect of filter and board info

--- app/assets/javascripts/shared/filter/sidebar_ui.js
+++ app/assets/javascripts/shared/filter/sidebar_ui.js
@@ -1,10 +1,7 @@
 $(document).on('turbolinks:load', function() {
-  $(document).on('click', '[data-single-board]', function(e){
-    $("[filter-and-boards]").addClass("d-none");
-    $("[board-info]").removeClass("d-none");
-  });
+
   $(document).on('click', '[return-filter]', function(e){
-    $("[filter-and-boards]").removeClass("d-none");
-    $("[board-info]").addClass("d-none");
+    $("[filter-and-boards]").removeClass("translateX-m100p");
+    $('[board-info]').addClass("translateX-100p");
   });
 });
