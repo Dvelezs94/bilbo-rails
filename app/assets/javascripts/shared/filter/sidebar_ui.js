6e85ac30be178150ef719ea3d746171892e7ee22
Mauricio Velez <mauriciovelsa99@gmail.com>
2021-08-09 16:00:52 -0500

    first design of board info, implementing design of edit campaign...

--- app/assets/javascripts/shared/filter/sidebar_ui.js
+++ app/assets/javascripts/shared/filter/sidebar_ui.js
@@ -1,6 +1,6 @@
 $(document).on('turbolinks:load', function() {
-
   $(document).on('click', '[return-filter]', function(e){
+    //transition effects
     $("[filter-and-boards]").removeClass("translateX-m100p");
     $('[board-info]').addClass("translateX-100p");
   });
