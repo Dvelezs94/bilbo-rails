eeec72f2a44eb2246426273391702aff61458b47
PinguiPit <pitpedrosalo@gmail.com>
2021-09-07 13:53:38 -0500

    divide statistics graph by bilbo

--- app/assets/javascripts/shared/legends_chartkick.js
+++ app/assets/javascripts/shared/legends_chartkick.js
@@ -1,18 +1,18 @@
 $(document).on('turbolinks:load', function() {
-  if ($("#chart-1").length) {
-    var chart = Chartkick.charts["chart-1"]
-    $("#chart-1").click(function(event) {
+  if ($("#daily-impressions-chart").length) {
+    var chart = Chartkick.charts["daily-impressions-chart"]
+    $("#daily-impressions-chart").click(function(event) {
       for (i = 0; i < chart.rawData.length; i++) {
         if (chart.chart.data.datasets[i]._meta[Object.keys(chart.chart.data.datasets[i]._meta)[0]].hidden == true) {
           valor = true;
         } else {
           valor = false;
         }
-        Chartkick.charts["chart-2"].chart.data.datasets[i].hidden = valor;
-        Chartkick.charts["chart-3"].chart.data.datasets[i].hidden = valor;
+        Chartkick.charts["daily-invested-chart"].chart.data.datasets[i].hidden = valor;
+        Chartkick.charts["peak-hours-chart"].chart.data.datasets[i].hidden = valor;
       }
-      Chartkick.charts["chart-2"].chart.update();
-      Chartkick.charts["chart-3"].chart.update();
+      Chartkick.charts["daily-invested-chart"].chart.update();
+      Chartkick.charts["peak-hours-chart"].chart.update();
     });
   }
 });
