d518933a26c3a81dfcebc715f6d92d1626b4c178
Carlos0914 <carloslop0914@gmail.com>
2021-10-06 12:53:27 -0500

    Do not repeat uploads on rake task for conversion, delete unused map_photos

--- app/models/map_photo.rb
+++ app/models/map_photo.rb
@@ -2,4 +2,12 @@ class MapPhoto < ApplicationRecord
   include ContentUploader::Attachment(:image)
 
   has_many :board_map_photos, dependent: :delete_all
+
+  def delete_if_not_in_use
+    # After deleting a BoardMapPhoto object, check if there is any other object of the same class using
+    # the same MapPhoto, and if it does, keep the photo (do nothing), or else delete the MapPhoto
+    if !BoardMapPhoto.where(map_photo: self).present?
+      self.delete
+    end
+  end
 end
