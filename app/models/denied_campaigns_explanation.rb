a31af09819b6793098b799bead6c542a97702c07
PinguiPit <pitpedrosalo@gmail.com>
2021-10-06 11:38:31 -0500

    denied campaign explanation

--- app/models/denied_campaigns_explanation.rb
+++ app/models/denied_campaigns_explanation.rb
@@ -1,4 +1,4 @@
 class DeniedCampaignsExplanation < ApplicationRecord
   belongs_to :boards_campaigns
-  enum message: { ilegal: 0, restricted_ad: 1, unpermitted: 2 }
+  enum message: { inappropriate_content: 0, too_long_short: 1, budget_very_low: 2, message_not_appropiate: 3, resolution_very_low: 4, choosen_dates_not_aviable: 5 }
 end
