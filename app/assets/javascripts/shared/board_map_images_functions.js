9a071a966aed669532468d61f1a5af79ec06340f
Carlos0914 <carloslop0914@gmail.com>
2021-10-08 12:38:18 -0500

    WIP: Show project map images on form to select exising ones instead of creating new objects

---
+++ app/assets/javascripts/shared/board_map_images_functions.js
@@ -0,0 +1,37 @@
+function showMapPhotos(selected_ids, board_slug){
+  $.ajax({
+    url:  "/board_map_photos/get_selected_map_photos",
+    dataType: "script",
+    data: {selected_photos: selected_ids.val(), board_slug: board_slug},
+    success: function(data) {
+    },
+    error: function(data) {
+      alert("Oops.. Ocurrio un error..");
+    }
+  });
+}
+
+function selectMapPhotos() {
+  // choose content in wizard
+    var items = $('div[data-content][data-processed="true"]');
+    // clear event handler on click and reinitialize
+    items.off("click");
+    items.on("click", function(e) {
+      e.preventDefault();
+      var content_id = $(this).attr("data-content")
+      // Click on nearest checkbox
+      $("#pickContent" + content_id).prop("checked", !$("#pickContent" + content_id).prop("checked"));
+      //append_content_live();
+    });
+
+    if ($('#board_map_photos').val().split(" ").length) {
+      selected_contents_ids = $('#board_map_photos').val().split(" ");
+      var i;
+      for (i = 0; i < selected_contents_ids.length; i++) {
+        selected_ad = selected_contents_ids[i]
+        if(!$("#pickContent" + selected_ad).prop("checked")){
+          $("#pickContent" + selected_ad).prop("checked", !$("#pickContent" + selected_ad).prop("checked"));
+        }
+      }
+    }
+}
